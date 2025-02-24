0d92e80bca23d21f3c2c9a07b5de16705ac542f5
Nicholas Poh <nicholasaws118@gmail.com>
2025-02-24 22:05:22 +0800

    Create basic room API

---
+++ backend/src/controllers/roomController.js
@@ -0,0 +1,101 @@
+const roomModel = require('../models/roomModel');
+const { use } = require('../routes/roomRoutes');
+
+// Get all rooms
+const getAllRooms = async (req, res) => {
+    try {
+        const rooms = await roomModel.getAll();
+        res.json(rooms);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+// Get a room by ID
+const getRoomById = async (req, res) => {
+    try {
+        const roomId = parseInt(req.params.id, 10);
+        const room = await roomModel.getById(roomId);
+        if (!room) {
+            return res.status(404).json({ message: 'room not found' });
+        }
+        res.json(room);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+// Get a room by ID
+const getRoomByName = async (req, res) => {
+    try {
+        const name = req.body;
+        const room = await roomModel.getByName(name);
+        if (!room) {
+            return res.status(404).json({ message: 'Room not found' });
+        }
+        res.json(room);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+// Create a new room
+const createRoom = async (req, res) => {
+    try {
+        console.log( req.body);
+        const { roomName, capacity, room_type, buildingName, is_active} = req.body;
+
+        if (!roomName || !buildingName || !capacity || !room_type) {
+            return res.status(400).json({ message: 'Missing required fields' });
+        }
+
+        const exsitingName = await roomModel.getByName(roomName);
+        if(exsitingName)
+        {
+            return res.status(400).json({ message: 'Room name is already in use' });
+        }
+        const newroom = await roomModel.create({ roomName, capacity, room_type, buildingName, is_active });
+        res.status(201).json({ message: 'room created successfully', room: newroom });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+// Update Room
+const updateRoom = async (req, res) => {
+    try {
+        const { roomName, capacity, room_type, buildingName, is_active } = req.body;
+        const updatedroom = await roomModel.update(req.params.id, { roomName, capacity, room_type, buildingName, is_active});
+
+        if (!updatedroom) {
+            return res.status(404).json({ message: 'room not found' });
+        }
+
+        res.json({ message: 'room updated successfully', room: updatedroom });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+// Delete room
+const deleteRoom = async (req, res) => {
+    try {
+        const deleted = await roomModel.deleteRoom(req.params.id);
+        if (!deleted) {
+            return res.status(404).json({ message: 'room not found' });
+        }
+        res.json({ message: 'room deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+module.exports = {
+    getAllRooms,
+    getRoomById,
+    getRoomByName,
+    createRoom,
+    updateRoom,
+    deleteRoom
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const pool = require('./config/db'); // PostgreSQL connection
 const userRoutes = require('./routes/userRoutes');
+const roomRoutes = require('./routes/roomRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/rooms', roomRoutes);
 
 // Set up a basic route
 app.get('/', (req, res) => {
