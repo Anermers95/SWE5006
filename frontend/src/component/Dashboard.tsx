f9f4fc3f807373e3bfc49b4652b3c58d2d84e6a5
EdwardF <edwardfoosw@gmail.com>
2025-02-27 14:10:44 +0800

    dashboard depending on user
    dashboard depending on user

--- frontend/src/component/Dashboard.tsx
+++ frontend/src/component/Dashboard.tsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate} from "react-router-dom";
 import Navbar from "./navbar";
 
 const Dashboard = () => {
+    const [userRole,setUserRole] = useState(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        
+        if (storedUser) {
+          const user = JSON.parse(storedUser);
+          setUserRole(user.role_id);  
+        } else {
+          window.location.href = "/login";
+        }
+      }, []);
+
     return (
         <div className="flex flex-col w-full min-h-screen">
             <div className="flex justify-end">
@@ -10,7 +23,7 @@ const Dashboard = () => {
             </div>
             
             <div className="flex-1 flex justify-center items-center">
-                <h1>Dashboard</h1>
+                <h1>{userRole === 1 ? "Admin Dashboard" : "User Dashboard"}</h1>
             </div>
         </div>
     )
