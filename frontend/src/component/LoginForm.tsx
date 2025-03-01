a8f5fcb7609616fe483e78fcebe8659b861d3997
Calvert Sim Wei Xiang <cal_9966@hotmail.com>
2025-03-01 18:50:09 +0800

    Frontend login text color fix

--- frontend/src/component/LoginForm.tsx
+++ frontend/src/component/LoginForm.tsx
@@ -87,7 +87,7 @@ const LoginForm = () => {
         <form onSubmit={handleSubmit}>
           <div className="w-full mt-6">
             <input
-              className="block w-full px-6 py-3 mt-2 text-lg text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+              className="block w-full px-6 py-3 mt-2 text-lg text-white placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
               type="email"
               placeholder="Email Address"
               aria-label="Email Address"
@@ -99,7 +99,7 @@ const LoginForm = () => {
 
           <div className="w-full mt-6">
             <input
-              className="block w-full px-6 py-3 mt-2 text-lg text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+              className="block w-full px-6 py-3 mt-2 text-lg text-white placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
               type="password"
               placeholder="Password"
               aria-label="Password"
