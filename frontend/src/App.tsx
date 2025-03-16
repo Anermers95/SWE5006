5a3c4c4a9e068daa3af78ed6b861dfc7f054f9e7
Nicholas Poh <nicholasaws118@gmail.com>
2025-03-16 12:42:31 +0800

    Manage Room and Create Room basic frontend (No validations)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,9 @@ import RegisterForm from './component/RegisterForm' // Import Register Page comp
 import HomePage from './component/HomePage'
 import Dashboard from './component/Dashboard'
 import RoomListings from './component/BookRoom'
+import RoomManagement from './component/RoomManagement'
+import RoomForm from './component/RoomForm'
+
 //ProtectedRoute
 import ProtectedRoute from './security/ProtectedRoute'
 
@@ -20,6 +23,11 @@ function App() {
         {/* Protect the route */}
         <Route path = "/dashboard" element={<ProtectedRoute element={<Dashboard />} />}/>
         <Route path="/book" element={<RoomListings />} />
+        <Route path="/room" element={<RoomManagement />} />
+        <Route path="/room/create" element={<RoomForm />} />
+
+
+
 
       </Routes>
     </Router>
---
+++ frontend/src/component/RoomForm.tsx
@@ -0,0 +1,107 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./navbar";
+import axios from "axios";
+
+const RoomForm = () => {
+  const navigate = useNavigate(); // Use navigate hook
+
+  const [formData, setFormData] = useState({
+    roomName: "",
+    capacity: "",
+    room_type: "",
+    buildingName: "",
+    is_active: true,
+  });
+
+  // Handle input changes
+  const handleChange = (e: any) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3000/rooms", formData);
+      console.log(response.data);
+      alert("Room Created Successful!");
+      navigate("/room");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  return (
+    
+    <div className="max-w-2xl mx-auto">
+      <form onSubmit={handleSubmit}>
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="roomName"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=""
+            value={formData.roomName}
+            onChange={handleChange}
+            required />
+
+          <label htmlFor="roomName" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Room Name</label>
+        </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="capacity"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            value={formData.capacity}
+            onChange={handleChange}
+            placeholder=""
+            required />
+
+          <label htmlFor="capacity" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Capacity</label>
+        </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="room_type"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            value={formData.room_type}
+            onChange={handleChange}
+            placeholder=""
+            required />
+          <label htmlFor="room_type" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Room Type</label>
+        </div>
+
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="buildingName"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            value={formData.buildingName}
+            onChange={handleChange}
+            placeholder=""
+            required />
+          <label htmlFor="buildingName" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Building Name</label>
+        </div>
+
+        <div className="flex items-center items-start mb-4">
+          <input
+            id="heckbox-1"
+            aria-describedby="checkbox-1"
+            type="checkbox"
+            className="bg-gray-50 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded"
+          />
+          <label htmlFor="checkbox-1" className="text-sm ml-3 font-medium text-gray-900">Is Active</label>
+        </div>
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+      </form>
+
+      <p className="mt-5">Check out the original floating label form elements on <a className="text-blue-600 hover:underline"
+        href="#" target="_blank">Flowbite</a> and browse other similar components built with Tailwind CSS.
+      </p>
+    </div>
+
+  );
+};
+
+export default RoomForm;
